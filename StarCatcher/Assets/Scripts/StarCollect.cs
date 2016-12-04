f38e0f31bc80b9384060bf034002e6c4f30e1b14
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-03 19:45:32 -0700

    StarPrefab Prototype
    added scoreboard, points add successfully
    prefab can be collected and deactivates after
    calls event on collection that adds points, value is unique to each instance

---
+++ StarCatcher/Assets/Scripts/StarCollect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarCollect : MonoBehaviour {
+
+    public int sValue;
+    public GameObject star;
+
+    public delegate int StarCollected(int val);
+    public static event StarCollected OnCollection;
+
+    void OnTriggerEnter() {
+        Debug.Log("hit");
+        if (OnCollection != null) {
+            OnCollection(sValue);
+        }
+        star.SetActive(false); //deactivate the star after collection
+    }
+
+}
