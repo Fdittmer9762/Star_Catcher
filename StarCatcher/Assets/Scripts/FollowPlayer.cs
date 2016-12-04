c9c6a178c7f9991f8fa24eecf7461d33eef12245
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-03 18:43:42 -0700

    Player Tracking/Following
    NavMeshAgents may now follow the player
    NavMeshAgents work well with children, no issues with rotations and odd transforms
    **need to integrate action event

--- StarCatcher/Assets/Scripts/FollowPlayer.cs
+++ StarCatcher/Assets/Scripts/FollowPlayer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
 
+    public NavMeshAgent navAgent;
+    public Transform playerAgent;
+
     void OnEnable() {
         PlayerMovement.PlayerMoved += OnPlayerMove;
     }
@@ -11,7 +14,9 @@ public class FollowPlayer : MonoBehaviour {
         PlayerMovement.PlayerMoved -= OnPlayerMove;
     }
 
-    void OnPlayerMove() {
-        Debug.Log("Tracking Player" + Time.time);
+    void OnPlayerMove() {  //                                   **need to find a way to feed info through the delegate, may be lest costly, thinking action<t>**
+        //Debug.Log("Tracking Player" + Time.time); //for debugging
+        navAgent.destination = playerAgent.position; //updates player position
+        navAgent.updateRotation = false; //prevents agent turning around messing with the sprites, cameras, etc.
     }
 }
