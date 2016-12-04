f29130a39da932fb8b3227d52a3256b1357b199b
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-03 21:10:16 -0700

    StarManager Prototype1
    created array of stars to be spawned WORKING
    **try tying the distance of next spawn to player speed
    **try setting delay with an event, wait until a star has been disabled before "spawning" more

---
+++ StarCatcher/Assets/Scripts/StarManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarManager : MonoBehaviour {
+
+
+    public GameObject[] Stars;
+    public Vector3 startPos;
+    public int delayMin, delayMax, delay;
+
+    void Start() {
+        startPos.y += 6;
+        StartCoroutine(StarSpawning(startPos));
+    }
+
+    IEnumerator StarSpawning(Vector3 rndPos) { //working may change, dont like current iteration, need to try some changes
+        for (int i = 0; i < Stars.Length; i++) {
+            yield return new WaitForSeconds(delay);
+            rndPos += new Vector3(Random.Range(0,5), 0,0);
+            Stars[i].transform.position = rndPos;
+            Stars[i].SetActive(true);
+        }
+        StartCoroutine(StarSpawning(rndPos));
+    }
+}
