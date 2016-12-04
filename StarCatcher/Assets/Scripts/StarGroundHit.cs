26e97b0ad83a41e648cedb3a2cafe971d6b77291
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-03 20:43:27 -0700

    Small Changes to StarPrefab

---
+++ StarCatcher/Assets/Scripts/StarGroundHit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarGroundHit : MonoBehaviour {
+
+    void OnTriggerEnter() {
+        Debug.Log("StarHitGround");
+        //emit particles
+        //start stardeath anim, anim will disable star
+        //
+    }
+
+}
