e518be39cd40a099209a52ec011b291a40a62236
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-03 18:16:59 -0700

    PlayerTrackingEvent
    created player tracking event
    **will feed player location to subs

---
+++ StarCatcher/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    void OnEnable() {
+        PlayerMovement.PlayerMoved += OnPlayerMove;
+    }
+
+    void OnDisable() {
+        PlayerMovement.PlayerMoved -= OnPlayerMove;
+    }
+
+    void OnPlayerMove() {
+        Debug.Log("Tracking Player" + Time.time);
+    }
+}
