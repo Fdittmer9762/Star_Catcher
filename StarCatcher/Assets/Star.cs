4be7494be8df3334007425be3d86e2ec385742c8
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-11 18:23:48 -0700

    Added Stars
    Added star trail
    added star bounce
    added star sprite

---
+++ StarCatcher/Assets/Star.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+    public Rigidbody starRB;
+    public float rnd, rndMax, rndMin;
+
+    void Start()
+    {
+        starRB = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable() {
+        Debug.Log("Fire!!!");
+        rnd = Random.Range(rndMin,rndMax);
+        starRB.AddForce(rnd, - Mathf.Abs(rnd), 0);
+    }
+
+}
