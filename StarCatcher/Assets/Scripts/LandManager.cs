8285dfa1b9355592320dc76a8fc46039c69261c5
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-17 23:51:53 -0700

    LandModule Recycling
    landmoduls working 
    land recycling working

---
+++ StarCatcher/Assets/Scripts/LandManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandManager : MonoBehaviour {
+
+    public GameObject newLandTrigger;
+    public GameObject[] LandModules;
+    int attemptLimit = 0;
+
+    void OnTriggerEnter() {
+        MoveLand();
+        Debug.Log("trigger");
+    }
+
+    void MoveLand() {
+        int rndNum = Random.Range(0, LandModules.Length);
+        switch (LandModules[rndNum].gameObject.activeInHierarchy) {
+            case true:
+                attemptLimit++;
+                if (attemptLimit <= LandModules.Length) {
+                    MoveLand();
+                }
+                break;
+            case false:
+                LandModules[rndNum].transform.position = newLandTrigger.transform.position;
+                LandModules[rndNum].SetActive(true);
+                newLandTrigger.transform.Translate(Vector3.right * Statics.landModuleSize);
+                attemptLimit = 0;
+                break;
+        }
+    }
+
+}
--- StarCatcher/Assets/Scripts/Statics.cs
+++ StarCatcher/Assets/Scripts/Statics.cs
@@ -7,4 +7,5 @@ public static class Statics {
     public static float gravity = 2.45f;
     public static float speedDamper = 0;
     public static float playerJumpForce = 40;
+    public static float landModuleSize = 20f;
 }
